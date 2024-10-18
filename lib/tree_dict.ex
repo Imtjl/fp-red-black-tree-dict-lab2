2dc286b4a39e208505e0fd06cd4502be791b79e4
worthant <b_dvorkin@niuitmo.ru>
2024-10-19 02:52:03 +0300

    :hammer: feat: Init project

---
+++ lib/tree_dict.ex
@@ -0,0 +1,18 @@
+defmodule TreeDict do
+  @moduledoc """
+  Documentation for `TreeDict`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TreeDict.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tree_dict_test.exs
@@ -0,0 +1,8 @@
+defmodule TreeDictTest do
+  use ExUnit.Case
+  doctest TreeDict
+
+  test "greets the world" do
+    assert TreeDict.hello() == :world
+  end
+end
