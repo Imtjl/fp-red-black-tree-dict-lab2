2ae1854c3b4fa52002e788ddb769e70b888d336d
worthant <b_dvorkin@niuitmo.ru>
2024-10-19 03:22:40 +0300

    :hammer: chore(deps): Update dependencies

--- mix.exs
+++ mix.exs
@@ -21,8 +21,9 @@ defmodule TreeDict.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:propcheck, "~> 1.4.1", only: [:test]},
+      {:excoveralls, "~> 0.14", only: :test}
     ]
   end
 end
