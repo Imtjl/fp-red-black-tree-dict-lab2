251b2a0b52d39faa28c562d80194ed7d16d4f3e3
worthant <b_dvorkin@niuitmo.ru>
2024-10-19 03:51:33 +0300

    :hammer: feat(mix): Update project tasks

--- mix.exs
+++ mix.exs
@@ -7,7 +7,17 @@ defmodule TreeDict.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+      preferred_cli_env: [
+        # Test coverage
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ],
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -25,4 +35,7 @@ defmodule TreeDict.MixProject do
       {:excoveralls, "~> 0.18", only: :test}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
