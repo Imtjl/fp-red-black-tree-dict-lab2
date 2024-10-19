f0a96efdd5a88348a63444dee266982d9add5890
worthant <b_dvorkin@niuitmo.ru>
2024-10-19 19:34:44 +0300

    :hammer: feat(lib): Init TreeDict interface - new & insert & get

--- lib/tree_dict.ex
+++ lib/tree_dict.ex
@@ -1,22 +1,46 @@
 defmodule TreeDict do
   @moduledoc """
-  Documentation for `TreeDict`.
+  Dictionary, implemented using Red-Black Tree
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TreeDict.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 
   defstruct tree: nil
 
-  def new, do: %TreeDict{}
+  @type t :: %TreeDict{tree: RedBlackTree.t() | nil}
+
+  @spec new() :: t()
+  def new do
+    %TreeDict{}
+  end
+
+  @spec insert(t(), any(), any()) :: t()
+  def insert(%TreeDict{tree: tree} = dict, key, value) do
+    new_tree = RedBlackTree.insert(tree, key, value)
+    %TreeDict{dict | tree: new_tree}
+  end
+
+  @spec get(t(), any()) :: any() | nil
+  def get(%TreeDict{tree: tree}, key) do
+    RedBlackTree.get(tree, key)
+  end
+
+  # def insert(%TreeDict{tree: tree} = dict, key, value) do
+  #   %TreeDict{tree: insert_into_tree(tree, key, value)}
+  # end
+
+  # filter, mapping, folding
+  # def filter(dict, func) do
+  #   %TreeDict{tree: RedBlackTree.filter(dict.tree, func)}
+  # end
+  #
+  # def map(dict, func) do
+  #   %TreeDict{tree: RedBlackTree.map(dict.tree, func)}
+  # end
+  #
+  # def foldl(dict, acc, func) do
+  #   RedBlackTree.foldl(dict.tree, acc, func)
+  # end
+  #
+  # def foldr(dict, acc, func) do
+  #   RedBlackTree.foldr(dict.tree, acc, func)
+  # end
 end
