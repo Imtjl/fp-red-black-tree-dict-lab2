35214c06678ab746d8bb623b73dc6ad8092460f7
worthant <b_dvorkin@niuitmo.ru>
2024-11-02 02:35:27 +0300

    :test_tube: test(lib): Test rb-tree folding

---
+++ test/tree_fold_test.exs
@@ -0,0 +1,45 @@
+defmodule RedBlackTreeFoldTest do
+  use ExUnit.Case
+  alias RedBlackTree
+
+  test "left fold processes nodes in order" do
+    tree = RedBlackTree.insert(nil, 3, "three")
+    tree = RedBlackTree.insert(tree, 1, "one")
+    tree = RedBlackTree.insert(tree, 2, "two")
+    # after this, the tree should look like this:
+    #
+    #    2
+    #   / \
+    #  1   3
+    #
+
+
+    result = RedBlackTree.foldl(tree, [], fn k, v, acc -> [{k, v} | acc] end)
+    # folding should be 1, 2, 3, 
+    # BUT we prepend each element in accumulator:
+    # [{k, v} | acc] - it results in reverse-ordering
+    expected = [{3, "three"}, {2, "two"}, {1, "one"}]
+
+    assert result == expected
+  end
+
+  test "right fold processes nodes in reverse order" do
+    tree = RedBlackTree.insert(nil, 3, "three")
+    tree = RedBlackTree.insert(tree, 1, "one")
+    tree = RedBlackTree.insert(tree, 2, "two")
+    # after this, the tree should look like this:
+    #
+    #    2
+    #   / \
+    #  1   3
+    #
+
+    result = RedBlackTree.foldr(tree, [], fn k, v, acc -> [{k, v} | acc] end)
+    # folding should be 1, 2, 3, 
+    # BUT we prepend each element in accumulator:
+    # [{k, v} | acc] - it results in reverse-ordering
+    expected = [{1, "one"}, {2, "two"}, {3, "three"}]
+
+    assert result == expected
+  end
+end
